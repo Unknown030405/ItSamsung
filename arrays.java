4feb6ca25a6a0977fee3a0847b12bbd0dad6b296
Unknown030405 <DPSdanilPar@gmail.com>
2019-09-20 22:43:26 +0500

    Update arrays.java

--- arrays.java
+++ arrays.java
@@ -2,61 +2,37 @@ import java.util.*;
 
 public class arrays {
 	public static void main(String[] args) {
-		int n;
-		while(true) {
-			System.out.println("Enter number (length of an array): ");
-			n = ask();
-			if (n != -1){
-				System.out.println("Generating...");
-				break;
+		int[] arr = new int[12];
+		arr = randomarr();
+		int mx = 0;
+		int ind_mx = 0;
+		for(int i = 0; i < 12; i++) {
+			if(arr[i] >= mx) {
+				mx = arr[i];
+				ind_mx = i;
 			}
-			System.out.println("Error! Number shuld be more, than 3!");
 		}
-		int[] arr;
-		arr = RandintArray(n);
+		System.out.println(mx);
+		System.out.println(ind_mx);
 		for(int i: arr) {
-			System.out.print(i + " ");
-		}
-		int[] arr1;
-		arr1 = evenFromArrays(arr,n);
-		System.out.println(' ');
-		for(int i: arr1) {
-			if(i == -1) {
-				break;
-			}
-			System.out.print(i + " ");
+			System.out.print(i);
+			System.out.print(' ');
 		}
 	}
-	private static int[] evenFromArrays(int[] arr, int n) {
-		int[] even = new int[n];
-		int c = 0;
-		for(int i = 0; i < n; i++) {
-			even[i] = -1;
-		}
-		for(int i: arr) {
-			if(i%2 == 0) {
-				even[c] = i;
-				c++;
-			}
-		}
-		return even;
-	}
-	private static int[] RandintArray(int n) {
-		Random rand = new Random();
-		int[] ar = new int[n];
-		for (int i = 0; i < n; i++) {
-			ar[i] = rand.nextInt(n);
+	static int[] randomarr() {
+		int[] ar = new int[12];
+		for(int i = 0; i < 12; i++) {
+			ar[i] = randint();
 		}
 		return ar;
 	}
-	static int ask() {
-		Scanner in = new Scanner(System.in);
-		int n = in.nextInt();
-		if(n<3) {
-			return -1;
+	static int randint() {
+		Random rand = new Random();
+		int i = rand.nextInt(16);
+		int a = rand.nextInt(2);
+		if (a == 0) {
+			return i;
 		}
-		else {
-			return n;
-		}
+		return -i;
 	}
 }
